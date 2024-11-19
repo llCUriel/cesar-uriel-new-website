402f67f497bec933af8fd79845334f0946399ed0
César Uriel <48939025+llCUriel@users.noreply.github.com>
2024-11-18 23:26:21 -0600

    fix nationality

--- data/personalInfo.js
+++ data/personalInfo.js
@@ -2,7 +2,7 @@ module.exports = [
   { meta: "first name", metaInfo: "Cesar" },
   { meta: "last name", metaInfo: "Hernandez" },
   { meta: "Age", metaInfo: "26 Years" },
-  { meta: "Nationality", metaInfo: "Mexico" },
+  { meta: "Nationality", metaInfo: "Mexican" },
   { meta: "Freelance", metaInfo: "Not available"},
   { meta: "Address", metaInfo: "Mexico City"},
   { meta: "phone", metaInfo: "+525572825823"},
